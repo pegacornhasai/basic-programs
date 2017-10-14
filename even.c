b4c4c0d2ed456bde9ef6d504e0da34c9bca5d1f4
sravani <pegacorn@sravanis-MacBook-Pro.local>
2017-10-14 19:54:30 +0530

    even program added

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{  
+    int n;
+    printf("enter a number\n");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("yes\n");
+    else printf("no\n");
+    return(0);
+
+
+
+}
